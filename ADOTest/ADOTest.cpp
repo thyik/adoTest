a92ec9e915689814f032d90ee66db9b221cea7d5
yikth <yikth@mit.com.sg>
2020-03-24 08:51:44 +0800

    test join sql

--- ADOTest/ADOTest.cpp
+++ ADOTest/ADOTest.cpp
@@ -10,8 +10,10 @@ int _tmain(int argc, _TCHAR* argv[])
     CADODatabase AdoDb;
     CString strConnection = _T("");
 #ifdef _WIN64
+    //strConnection = _T("Provider=Microsoft.ACE.OLEDB.12.0;Persist Security Info=False;"\
+    //         "Data Source=D:\\Machine\\Database\\MitPkg.mdb");
     strConnection = _T("Provider=Microsoft.ACE.OLEDB.12.0;Persist Security Info=False;"\
-             "Data Source=D:\\Machine\\Database\\MitPkg.mdb");
+             "Data Source=D:\\Machine\\Database\\MitMc.mdb");
 #else
     // for 32-bit process
     strConnection = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\\Machine\\Database\\MitPkg.mdb";
@@ -24,7 +26,10 @@ int _tmain(int argc, _TCHAR* argv[])
         CADORecordset rs(&AdoDb);
 
 //        if(rs.Open("SELECT * FROM [MotorSpeed] ORDER BY [Package Name]", CADORecordset::openQuery))
-        if(rs.Open("SELECT * FROM [MotorSpeed] WHERE [Package Name] = \"Default\"", CADORecordset::openQuery))
+        //if(rs.Open("SELECT * FROM [MotorSpeed] WHERE [Package Name] = \"Default\"", CADORecordset::openQuery))
+        if(rs.Open("SELECT Motor.*, MotorSubField.SubFieldPosName FROM MotorSubField "
+                     "RIGHT JOIN Motor ON MotorSubField.[Position Name] = Motor.[Position Name] "
+                     "ORDER BY Motor.ID, MotorSubField.ID", CADORecordset::openQuery))
         {
             //
             CString xmlFile = "D:\\Temp\\test.xml";
@@ -36,9 +41,12 @@ int _tmain(int argc, _TCHAR* argv[])
             {
                 CString csValue;
                 double homeMaxSpd;
-                rs.GetFieldValue("Package Name", csValue);
+                //rs.GetFieldValue("Package Name", csValue);
+                rs.GetFieldValue("Module Name", csValue);
                 rs.GetFieldValue("Motor Name", csValue);
-                rs.GetFieldValue("Home Max Speed", homeMaxSpd);
+                rs.GetFieldValue("Position Name", csValue);
+                rs.GetFieldValue("SubFieldPosName", csValue);
+                //rs.GetFieldValue("Home Max Speed", homeMaxSpd);
                 // eg. : add, update
                 //rs.AddNew();
                 //rs.SetFieldValue(0, "dataU");
@@ -57,7 +65,7 @@ int _tmain(int argc, _TCHAR* argv[])
       //.
       //.
       //.
-        BOOL test = AdoDb.Execute("select * from MotorSpeed order by [Package Name]");
+        //BOOL test = AdoDb.Execute("select * from MotorSpeed order by [Package Name]");
 
         AdoDb.Close();
     }
